6cc3b9e7b47b69cb4fb27fdfb6abffef81314655
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-08-24 19:15:46 -1000

    index route configured

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Index from "../pages/Index";
 import Show from "../pages/Show";
 
 function Main(props) {
+  const [ records, setRecords ] = useState(null);
+  const API_URL = "http://localhost:4000/api/records";
+
+  const getRecords = async () => {
+    try{
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setRecords(data);
+    } catch (error) {
+      //TODO: add logic or task here
+    }
+  };
+
+  useEffect(() => {
+    getRecords();
+  }, []);
+
   return (
     <main>
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/" element={<Index records={records}/>} />
         <Route path="/records/:id" element={<Show />} />
       </Routes>
     </main>
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+
 function Index(props) {
-    return <h1>Index</h1>;
-  }
-  
-  export default Index;
+
+  const loaded = () => {
+    return props.records.map(({ bandName, albumTitle, image, _id }) => {
+      return (
+        <div className="record" key={_id}>
+          <Link to={`/records/${_id}`}>
+          <h1>{albumTitle}</h1>
+          </Link>
+          <img src={image} alt={albumTitle} />
+          <h3>{bandName}</h3>
+        </div>
+      )
+    })
+  };
+
+  const loading = () => {
+    return <h1>Loading...</h1>
+  };
+    
+
+return props.records ? loaded() : loading();
+}
+
+export default Index;
