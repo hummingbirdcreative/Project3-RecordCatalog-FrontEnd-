58248bf26032846b0516c3dbfad8845433f3651e
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-08-24 17:33:56 -1000

    wip: configured header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Header(props) {
-    return <h1>Header</h1>;
-  }
-  
-  export default Header;
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <div>Record App</div>
+      </Link>
+    </nav>
+  );
+}
+
+export default Header;
