03b379a97e89d3c4eecae863a114d9c6a0b7748c
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-08-26 09:12:52 -1000

    wip: materialize basics added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
+import { Typography, AppBar, Toolbar, CssBaseline } from '@mui/material';
+import AlbumIcon from '@mui/icons-material/Album';
 
 function Header(props) {
   return (
-    <nav className="nav">
-      <Link to="/">
-        <div>Record App</div>
-      </Link>
-    </nav>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <AlbumIcon />
+          <Link to="/">
+            <Typography variant="h6">
+              Record Crate
+            </Typography>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 }
 
