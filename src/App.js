01f160e0ef6f8828f871c9d63f3942f4294084a4
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-08-27 08:55:52 -1000

    wip: user observer initialized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { auth, onAuthStateChanged } from "./firebase";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer"
 
 function App() {
+  const [ userState, setUserState ] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, user => setUserState(user));
+  }, []);
+
   return (
     <div className="App">
       <Header />
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -32,4 +32,4 @@ function logout () {
   return signOut(auth);
 }
 
-export { auth, login, logout }
+export { auth, login, logout, onAuthStateChanged }
